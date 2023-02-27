8e29fb7fb165aa7fa970e246ba91ea458ff8db5d
Macyostrander <macyostrander@gmail.com>
2023-02-27 10:24:04 -0500

    initialized motors and inputs, added funtion declarations, and created moveForward function

---
+++ main2.cpp
@@ -0,0 +1,58 @@
+#include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHRPS.h>
+
+
+//Declarations for encoders & motors
+AnalogInputPin CdS_cell (FEHIO::P0_1)
+DigitalEncoder right_encoder(FEHIO::P0_0);
+DigitalEncoder left_encoder(FEHIO::P0_1);
+FEHMotor right_motor(FEHMotor::Motor3,9.0);
+FEHMotor left_motor(FEHMotor::Motor0,9.0);
+DigitalInputPin micro_left(FEHIO::P0_1)
+DigitalInputPin micro_right(FEHIO::P0_1)
+
+void turnLeft(int a);
+void turnRight(int b);
+void moveForward(int counts,int percent);
+void moveBackward(int counts,int percent);
+void zero(int z);
+void boardingPass();
+
+
+void boardingPass(){
+
+}
+
+void moveForward(int counts){
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //turn motors off
+    zero();
+}
+
+void zero(){
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+}
+
+int main(){
+
+    //wait for light to turn on
+    while(CdS_cell.Value()<LIGHT_ON){}
+
+    
+}
