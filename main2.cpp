1d289f3a8a575098568f70a46a617e3aeb3d424a
Macyostrander <macyostrander@gmail.com>
2023-03-01 14:47:40 -0500

    fixed syntax issues

--- main2.cpp
+++ main2.cpp
@@ -6,13 +6,13 @@
 
 
 //Declarations for encoders & motors
-AnalogInputPin CdS_cell (FEHIO::P0_1)
+AnalogInputPin CdS_cell (FEHIO::P0_1);
 DigitalEncoder right_encoder(FEHIO::P0_0);
 DigitalEncoder left_encoder(FEHIO::P0_1);
 FEHMotor right_motor(FEHMotor::Motor3,9.0);
 FEHMotor left_motor(FEHMotor::Motor0,9.0);
-DigitalInputPin micro_left(FEHIO::P0_1)
-DigitalInputPin micro_right(FEHIO::P0_1)
+DigitalInputPin micro_left(FEHIO::P0_1);
+DigitalInputPin micro_right(FEHIO::P0_1);
 
 void turnLeft(int tcount, int tpercent);
 void turnRight(int tcount, int tpercent);
@@ -20,8 +20,6 @@ void moveForward(int counts,int percent);
 void moveBackward(int counts,int percent);
 void zero();
 void boardingPass();
-
-
 
 
 void moveForward(int counts, int percent){
@@ -71,16 +69,14 @@ int turnRight(int tcount, int tpercent)
     left_encoder.ResetCounts();
 
     // motor percents 
-    right_motor.SetPercent(tpercent);
-    left_motor.SetPercent(-tpercent);
+    right_motor.SetPercent(-tpercent);
+    left_motor.SetPercent(tpercent);
 
     // keep running motors while average of turn counts is less then tcount
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < tcount);
 
     // turn off motors
     zero();
-
-
 
 }
 
@@ -91,20 +87,29 @@ int turnLeft(int tcount, int tpercent)
     left_encoder.ResetCounts();
 
     // motor percents 
-    right_motor.SetPercent(-tpercent);
-    left_motor.SetPercent(tpercent);
+    right_motor.SetPercent(tpercent);
+    left_motor.SetPercent(-tpercent);
 
     // keep running motors while average of turn counts is less then tcount
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < tcount);
 
     // turn off motors
     zero();
-
-
-
 }
 
 int main(){
+
+    float x, y; //for touch screen
+
+    //Initialize the screen
+    LCD.Clear(BLACK);
+    LCD.SetFontColor(WHITE);
+
+    LCD.WriteLine("Test");
+    LCD.WriteLine("Touch the screen");
+    while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed  
+
 
     //count for turns. will turn 90 deg
     tcount= 225;
@@ -133,7 +138,7 @@ int main(){
     moveForward(240,percent);
 
     //robot is now facing away from kiosk, 90*
-    turnleft(tcount,tpercent);
+    turnLeft(tcount,tpercent);
 
     //13'
     moveBackward(520,percent);
@@ -149,11 +154,13 @@ int main(){
     //16'
     moveForward(640,percent);
 
+    //90*
     turnLeft(tcount,tpercent);
 
     //4'
     moveForward(160,percent);
 
+    //90*
     turnRight(tcount,tpercent);
 
     //down ramp, 12'
