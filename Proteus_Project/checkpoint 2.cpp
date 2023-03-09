dca89f8998aab62a9782c4d50a90e5fc89267694
Macyostrander <macyostrander@gmail.com>
2023-03-09 17:14:33 -0500

    added rps class

--- Proteus_Project/checkpoint 2.cpp
+++ Proteus_Project/checkpoint 2.cpp
@@ -16,6 +16,30 @@
 #define NOSE_X
 #define NOSE_Y
 
+// RPS Delay time
+#define RPS_WAIT_TIME_IN_SEC 0.35
+
+// Shaft encoding counts for CrayolaBots
+#define COUNTS_PER_INCH 40.5
+#define COUNTS_PER_DEGREE 2.48
+
+/* Defines for how long each pulse should be and at what motor power. 
+These value will normally be small, but you should play around with the values to find what works best */
+#define PULSE_TIME 0.25
+#define PULSE_POWER 20.0
+
+// Define for the motor power while driving (not pulsing)
+#define POWER 20.0
+
+#define HEADING_TOLERANCE 2
+
+/* Direction along axis which robot is traveling
+Examples:
+	- if robot is traveling to the upper level, that is a PLUS as the y-coordinate is increasing
+	- if robot is traveling to the lower level, that is a MINUS as the y-coordinate is decreasing
+*/
+#define PLUS 0
+#define MINUS 1
 
 //Declarations for encoders & motors
 AnalogInputPin CdS_cell (FEHIO::P1_0);
@@ -37,6 +61,176 @@ void turnOnlyLeft(int tcount, int tpercent);
 void turnOnlyRight(int tcount, int tpercent);
 void zero();
 void boardingPass(bool red);
+
+class RPS{
+
+    private:
+
+    public:
+
+    /*
+    * Pulse forward a short distance using time
+    */
+    void pulse_forward(int percent, float seconds)
+    {
+        // Set both motors to desired percent
+        right_motor.SetPercent(percent);
+        left_motor.SetPercent(percent);
+
+        // Wait for the correct number of seconds
+        Sleep(seconds);
+
+        // Turn off motors
+        right_motor.Stop();
+        left_motor.Stop();
+    }
+
+    /*
+    * Pulse counterclockwise a short distance using time
+    */
+    void pulse_counterclockwise(int percent, float seconds)
+    {
+        // Set both motors to desired percent
+        right_motor.SetPercent(percent);
+        left_motor.SetPercent(-percent);
+
+        // Wait for the correct number of seconds
+        Sleep(seconds);
+
+        // Turn off motors
+        right_motor.Stop();
+        left_motor.Stop();
+    }
+
+    /*
+    * Use RPS to move to the desired x_coordinate based on the orientation of the QR code
+    */
+    void check_x(float x_coordinate, int orientation)
+    {
+        // Determine the direction of the motors based on the orientation of the QR code
+        int power = PULSE_POWER;
+        if (orientation == MINUS)
+        {
+            power = -PULSE_POWER;
+        }
+
+        // Check if receiving proper RPS coordinates and whether the robot is within an acceptable range
+        while (RPS.X() > -1 && (RPS.X() < x_coordinate - 1 || RPS.X() > x_coordinate + 1))
+        {
+            if (RPS.X() < x_coordinate - 1)
+            {
+                // Pulse the motors for a short duration in the correct direction
+                pulse_forward(power, PULSE_TIME);
+            }
+            else if (RPS.X() > x_coordinate+1)
+            {
+                // Pulse the motors for a short duration in the correct direction
+                pulse_forward(-power, PULSE_TIME);
+            }
+            Sleep(RPS_WAIT_TIME_IN_SEC);
+        }
+    }
+
+    /*
+    * Use RPS to move to the desired y_coordinate based on the orientation of the QR code
+    */
+    void check_y(float y_coordinate, int orientation)
+    {
+        // Determine the direction of the motors based on the orientation of the QR code
+        int power = PULSE_POWER;
+        if (orientation == MINUS)
+        {
+            power = -PULSE_POWER;
+        }
+
+        // Check if receiving proper RPS coordinates and whether the robot is within an acceptable range
+        while (RPS.Y() > -1 && (RPS.Y() < y_coordinate - 1 || RPS.Y() > y_coordinate + 1))
+        {
+            if (RPS.Y() < y_coordinate - 1)
+            {
+                // Pulse the motors for a short duration in the correct direction
+                pulse_forward(power, PULSE_TIME);
+            }
+            else if (RPS.Y() > y_coordinate+1)
+            {
+                // Pulse the motors for a short duration in the correct direction
+                pulse_forward(-power, PULSE_TIME);
+            }
+            Sleep(RPS_WAIT_TIME_IN_SEC);
+        }
+    }
+
+
+    /*
+    * Use RPS to move to the desired heading
+    */
+    void check_heading(float heading)
+    {
+        // If the robot is at the desired heading(
+        if ((RPS.Heading() - heading > -HEADING_TOLERANCE) && (RPS.Heading() - heading < HEADING_TOLERANCE))
+        {
+            LCD.WriteLine("Robot at correct heading");
+        }
+        // If the robot is more than two (HEADING_TOLERANCE) to the right
+        while (RPS.Heading() - heading < -HEADING_TOLERANCE)
+        {
+            right_motor.SetPercent(PULSE_POWER);
+            Sleep(PULSE_TIME);
+        }
+
+        // If more than two (HEADING TOLERANCE) to the left
+        while (RPS.Heading() - heading > HEADING_TOLERANCE)
+        {
+            left_motor.SetPercent(PULSE_POWER);
+            Sleep(PULSE_TIME);
+        }
+
+        // You will need to fill out this one yourself and take into account
+        // checking for proper RPS data and the edge conditions
+        //(when you want the robot to go to 0 degrees or close to 0 degrees)
+        
+
+        /*
+            SUGGESTED ALGORITHM:
+            1. Check the current orientation of the QR code and the desired orientation of the QR code
+            2. Check if the robot is within the desired threshold for the heading based on the orientation
+            3. Pulse in the correct direction based on the orientation
+        */
+    }
+
+    int go()
+    {
+
+        // COMPLETE CODE HERE TO READ SD CARD FOR LOGGED X AND Y DATA POINTS
+        FEHFile *fptr = SD.FOpen("RPS_POIs.txt", "r");
+        SD.FScanf(fptr, "%f%f", &A_x, &A_y);
+
+
+        SD.FClose(fptr);
+
+        turn_90_counts = 240;
+        turn_180_counts = 480;
+
+        
+        // Open file pointer for writing
+        fptr = SD.FOpen("RESULTS.txt", "w");
+        
+        // COMPLETE CODE HERE TO WRITE EXPECTED AND ACTUAL POSITION INFORMATION TO SD CARD
+        SD.FPrintf(fptr, "Expected A Position: %f %f %f\n", A_x,A_y,A_heading);
+        SD.FPrintf(fptr, "Actual A Position:   %f %f %f\n\n", RPS.X(), RPS.Y(),RPS.Heading());
+
+        // B --> C
+        check_x(C_x, MINUS);
+        check_y();
+        check_heading();
+        Sleep(1.0);
+
+        // Close file pointer
+        SD.FClose(fptr);
+
+        return 0;
+    }
+}
 
 
 void moveForward(int counts, int percent){
@@ -252,11 +446,6 @@ void followLine(){
     }
 
     zero();
-
-    if(CdS_cell.Value()>1){
-        //still hasn't found light
-        while()
-    }
 }
 
 int main(){
@@ -313,7 +502,12 @@ int main(){
 
     //line following to light at nose of plane
     followLine();
-    
+
+    RPS position;
+    position.check_x();
+    position.check_y();
+    position.check_heading();
+
     //read value of CdS cell to determine boarding pass
     float val = CdS_cell.Value();
     bool red = (val<.55);
